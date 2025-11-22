0446202a213f9d31968b4ddaa7149aebf91b65b4
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-11-22 22:00:14 +0200

    02. usefull-things-406-cards. UI. Cards.

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const tracks = document.querySelectorAll(".carousel__content");
+
+tracks.forEach((track) => {
+    const cards = [...track.children];
+
+    for (const card of cards) {
+        track.appendChild(card.cloneNode(true));
+    }
+});
